1fead2bbb62267397fd68498ddb301933f584403
Nathan Martinez <nmartinez@arborhealth.com>
2015-11-12 13:48:09 -0600

    Customer view completed; Main page updated

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,22 @@
 /**
  * Created by nmartinez on 11/11/2015.
  */
+(function(){
+    'use strict';
+
+    var _templateBase = './scripts';
+
+    angular.module('app', [
+        'ngRoute',
+        'ngMaterial',
+        'ngAnimate'
+    ])
+    .config(['$routeProvider', function($routeProvider){
+            $routeProvider.when('/', {
+                templateUrl: _templateBase + '/customer/customer.html',
+                controller: 'customerController',
+                controllerAs: '_ctrl'
+            });
+            $routeProvider.otherwise({redirectTo: '/'});
+    }]);
+})();
